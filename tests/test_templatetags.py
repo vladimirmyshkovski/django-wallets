6293c48b85459b5afd11c29c9caaff2721547653
narnik <narnikgamarnikus@gmail.com>
2018-04-26 20:14:16 +0300

    added templatetags

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -24,4 +24,38 @@ class TestUnpaidUserSendedInvoices(TestCase):
             }
         ))
 
-        print(out)
+        self.assertEqual(
+            out,
+            '0'
+        )
+
+    def test_with_invoices(self):
+        btc = factories.BtcFactory(user=self.user)
+        ltc = factories.LtcFactory(user=self.user)
+        dash = factories.DashFactory(user=self.user)
+        doge = factories.DogeFactory(user=self.user)
+        bcy = factories.BcyFactory(user=self.user)
+        
+        factories.BtcInvoiceFactory(sender_wallet_object=btc)
+        factories.LtcInvoiceFactory(sender_wallet_object=ltc)
+        factories.DashInvoiceFactory(sender_wallet_object=dash)
+        factories.DogeInvoiceFactory(sender_wallet_object=doge)
+        factories.BcyInvoiceFactory(sender_wallet_object=bcy)
+
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.user = self.user
+
+        out = Template(
+            "{% load wallets_tags %}"
+            "{% unpaid_user_sended_invoices %}"
+        ).render(Context({
+            'request': request,
+            'symbol': 'bcy'
+            }
+        ))
+
+        self.assertEqual(
+            out,
+            '4'
+        )
