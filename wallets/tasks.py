a77ef58269359e5e5caecf0d15a20786343ee7d0
narnik <narnikgamarnikus@gmail.com>
2018-05-24 22:30:26 +0300

    added save Invoice after pay

--- wallets/tasks.py
+++ wallets/tasks.py
@@ -10,14 +10,16 @@ from .signals import invoice_is_paid
 
 @periodic_task(run_every=timedelta(seconds=settings.CHECK_EVERY_SECONDS))
 def check_transaction_confirmations():
+    print('hello')
     if settings.CHECK_TRANSACTION_CONFIRMATIONS:
         invoices = Invoice.objects.filter(is_paid=False, tx_ref__is_null=False)
         for invoice in invoices:
-            details = blockcypher.get_transaction_details(
-                tx_ref=invoice.tx_ref,
-                coin_symbol=invoice.wallet.coin_symbol
-            )
-            if details['confirmations'] >= settings.DEFAULT_CONFIRMATIONS:
-                invoice.is_paid = True
-                invoice.save()
-                invoice_is_paid.send(sender=Invoice, invoice_id=invoice.id)
+            if invoice.tx_ref:
+                details = blockcypher.get_transaction_details(
+                    tx_ref=invoice.tx_ref,
+                    coin_symbol=invoice.wallet.coin_symbol
+                )
+                if details['confirmations'] >= settings.DEFAULT_CONFIRMATIONS:
+                    invoice.is_paid = True
+                    invoice.save()
+                    invoice_is_paid.send(sender=Invoice, invoice_id=invoice.id)
